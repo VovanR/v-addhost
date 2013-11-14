3f3c384d3b2ac8cb65dfb9f2830cd3b24aa30ac4
VovanR <mail@vovanr.com>
2013-11-14 22:59:35 +0400

    Issue #4: Скрипт можно запускать от пользователя; Issue #3: Имя файла хостов берется от имени пользователя; Issue #2: Включает файл хостов, если он еще не подключен; Issue #1: Перезапускает апач после добавления хоста

--- v-addhost.sh
+++ v-addhost.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# v-addhost, version 0.5
+# v-addhost, version 0.6
 #
 # Скрипт добавляет новый хост в апач
 # Имя хоста передается в аргументе вызова скрипта
@@ -11,35 +11,20 @@
 
 # Цель:
 # <VirtualHost *:80>
-#     DocumentRoot    /sites/example/public
-#     ServerName      example
-#     ServerAlias     example.vovan
-#     ErrorLog        /sites/example/error.log
-#     CustomLog       /sites/example/access.log common
-#     <Directory "/sites/example/public">
-#         AllowOverride all
-#         Options FollowSymLinks
-#         Require all granted
-#     </Directory>
-# </VirtualHost>
-#
-# Случай с субдоменом:
-# <VirtualHost *:80>
-#     DocumentRoot    /sites/example/v2/public
-#     ServerName      v2.example
-#     ServerAlias     v2.example.vovan
-#     ErrorLog        /sites/example/v2/error.log
-#     CustomLog       /sites/example/v2/access.log common
-#     <Directory "/sites/example/v2/public">
-#         AllowOverride all
-#         Options FollowSymLinks
-#         Require all granted
-#     </Directory>
+# 	DocumentRoot /sites/example/public
+# 	ServerName   example
+# 	ServerAlias  example.vovan
+# 	ErrorLog     /sites/example/error.log
+# 	CustomLog    /sites/example/access.log common
+# 	<Directory "/sites/example/public">
+# 		Options FollowSymLinks
+# 		AllowOverride All
+# 		Require all granted
+# 	</Directory>
 # </VirtualHost>
 
-# Пример:
+# Пример использования:
 # addhosts example
-# addhosts v2.example
 
 
 
@@ -47,14 +32,16 @@
 ############
 
 # Директория с хостами
-sitesDir="/etc/apache2/sites-available/"
+sitesAvailableDir="/etc/apache2/sites-available"
+sitesEnabledDir="/etc/apache2/sites-enabled"
 
 # Файл с хостами
-sitesName="vovan"
-
+sitesName=$(whoami)".conf"
 # DNS zone (если настроена локальная зона)
 zone="vovan"
 
+# Директория проектов
+sitesDir="/sites"
 # Корень проекта (обычно 'www' или 'public')
 public="public"
 
@@ -64,88 +51,59 @@ public="public"
 ##########
 
 # Составляем путь до файла хостов
-sites=$sitesDir$sitesName".conf"
+sites=${sitesAvailableDir}"/"${sitesName}
+
 
 # Добавить точку перед зоной
-if [ -n "$zone" ];then
-    zone="."$zone
+if [ -n "${zone}" ]; then
+	zone="."${zone}
 fi
 
 # Если есть хотябы один аргумент
-if [ -n "$1" ];then
-    # Добавить пустую строку (для красоты)
-    echo "" >> $sites
+if [ -n "$1" ]; then
+	# Имя домена
+	domain=$1
 
-    # Разбить имя сайта на массив, если аргумент задан с субдоменом (v2.example)
-    site=(${1//./ })
-    # Показать весь массив
-    # echo ${site[@]}
-    # Показать отдельные элементы массива
-    # echo ${site[0]}
-    # echo ${site[1]}
+# Пишим в файл конфига
+sudo tee -a ${sites} << EOF
 
-    # Имя домена
-    domain=${site[0]}
+<VirtualHost *:80>
+	DocumentRoot ${sitesDir}/${domain}/${public}
+	ServerName   ${domain}
+	ServerAlias  ${domain}${zone}
+	ErrorLog     ${sitesDir}/${domain}/error.log
+	CustomLog    ${sitesDir}/${domain}/access.log common
+	<Directory "${sitesDir}/${domain}/${public}">
+		Options FollowSymLinks
+		AllowOverride All
+		Require all granted
+	</Directory>
+</VirtualHost>
+EOF
 
-    # Есть ли второй элемент массива
-    if [ -n "${site[1]}" ];then
-        domain=${site[1]}
-        subdomain=${site[0]}
-        echo "<VirtualHost *:80>" >> $sites
-        echo -e "\tDocumentRoot\t/sites/"$domain"/"$subdomain"/"$public >> $sites
-        echo -e "\tServerName\t"$subdomain"."$domain >> $sites
-        echo -e "\tServerAlias\t"$subdomain"."$domain$zone >> $sites
-        echo -e "\tErrorLog\t/sites/"$domain"/"$subdomain"/error.log" >> $sites
-        echo -e "\tCustomLog\t/sites/"$domain"/"$subdomain"/access.log common" >> $sites
-        echo -e "\t<Directory \"/sites/"$domain"/"$subdomain"/"$public"\">" >> $sites
-        echo -e "\t\tOptions FollowSymLinks" >> $sites
-        echo -e "\t\tAllowOverride all" >> $sites
-        echo -e "\t\tRequire all granted" >> $sites
-        echo -e "\t</Directory>" >> $sites
-        echo "</VirtualHost>" >> $sites
-    else
-        echo "<VirtualHost *:80>" >> $sites
-        echo -e "\tDocumentRoot\t/sites/"$domain"/"$public >> $sites
-        echo -e "\tServerName\t"$domain >> $sites
-        echo -e "\tServerAlias\t"$domain$zone >> $sites
-        echo -e "\tErrorLog\t/sites/"$domain"/error.log" >> $sites
-        echo -e "\tCustomLog\t/sites/"$domain"/access.log common" >> $sites
-        echo -e "\t<Directory \"/sites/"$domain"/"$public"\">" >> $sites
-        echo -e "\t\tOptions FollowSymLinks" >> $sites
-        echo -e "\t\tAllowOverride all" >> $sites
-        echo -e "\t\tRequire all granted" >> $sites
-        echo -e "\t</Directory>" >> $sites
-        echo "</VirtualHost>" >> $sites
-    fi
 else
-    echo -e "Error: Имя проекта кто писать будет?"
-    exit 2
-    # Return 2
-    # Exit Status: Incorrect usage
+	echo -e "Error: Имя проекта кто писать будет?"
+	exit 2
+	# Return 2
+	# Exit Status: Incorrect usage
 fi
 
 
-# Выполнение целевой команды
-# echo -e "sitesDir:\t\t"$sitesDir
-# echo -e "sitesName:\t\t"$sitesName
-# echo -e "sites:\t\t\t"$sites
+# Включаем файл хостов
+if [ ! -e ${sitesEnabledDir}"/"${sitesName} ]; then
+	if [ ! -e ${sites} ]; then
+		echo -e "Error: File \""${sites}"\" does not exist";
+		exit 2
+		# Return 2
+		# Exit Status: Incorrect usage
+	else
+		sudo a2ensite ${sitesName};
+	fi
+fi
 
 
-# Для отладки
-# echo -e "\n"
-# echo -e "script:\t"$0
-# echo -e "arguments.lenght:\t"$#
-# echo -e "удачна ли предыдущая комманда ( 0 - удачная ):\t"$?
-# echo -e "PID:\t"$$
-# echo -e "last PID:\t"$!
-# echo -e "all arguments:\t"$*
-# echo -e "all arguments2:\t"$@
-# echo -e "last argument of last sh:\t"$_
-
-# Для отладки
-# echo "" >> $sites
-# echo $(date) >> $sites
-# cat $sites
+# Перезапускаем апач
+sudo service apache2 restart
 
 
 echo -e "Successful execution"
